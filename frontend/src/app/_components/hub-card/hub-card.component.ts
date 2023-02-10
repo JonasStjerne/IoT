ba8ebb3f70d36bc6f725606e91bc176a537e4114
Jonas Stjerne <j.stjerne@live.dk>
2023-02-10 11:02:35 +0100

    Add confirm modal to delete hub

--- frontend/src/app/_components/hub-card/hub-card.component.ts
+++ frontend/src/app/_components/hub-card/hub-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { Hub } from 'src/app/_api/models';
 import { ApiService } from 'src/app/_api/services';
@@ -12,7 +13,8 @@ export class HubCardComponent implements OnInit {
   @Input() hub!: Hub;
   constructor(
     private apiService: ApiService,
-    private toastService: ToastrService
+    private toastService: ToastrService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {}
@@ -24,5 +26,22 @@ export class HubCardComponent implements OnInit {
         this.toastService.success('Hub name changed');
         console.log(res);
       });
+  }
+
+  deleteHub(content: TemplateRef<any>) {
+    this.modalService.open(content).result.then(
+      (result) => {
+        if (result === 'delete') {
+          this.apiService
+            .hubControllerUnRegister({ id: this.hub.id })
+            .subscribe((res) => {
+              this.toastService.success('Hub deleted');
+            });
+        }
+      },
+      (reason) => {
+        console.log(reason);
+      }
+    );
   }
 }
