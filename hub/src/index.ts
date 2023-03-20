813b34777217761b742723ecc32af1f233f6ec94
Jonas Stjerne <jost@trifork.com>
2023-03-20 13:28:59 +0100

    Move scheduling data

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -4,66 +4,77 @@ import { ActionRepeat } from "./models/action.dto";
 import { IWorkerDto, WorkerAction, WorkerState, WorkerStatus } from "./models/worker.dto";
 import { socketConnection } from "./websocket";
 
-const workerData: IWorkerDto[] = [
+const socket = socketConnection(
   {
-    id: "id",
-
-    name: "name",
-
-    action: WorkerAction.PRESS,
-
-    status: WorkerStatus.IDLE,
-
-    state: WorkerState.ONLINE,
-
-    actions: [
-      {
-        id: "action1",
-        repeat: ActionRepeat.ONCE,
-
-        executeDateTime: new Date(Date.now() + 5000),
-
-        durationSeconds: 1,
-
-        isComplete: false,
-      },
-    ],
+    backendHost: process.env.BACKEND_HOST!,
+    backendPort: +process.env.BACKEND_PORT!,
   },
-];
-const sch = new scheduler();
-sch.scheduleActions(workerData);
+  {
+    hubId: process.env.HUB_ID!,
+    hubSecret: process.env.HUB_SECRET!,
+  }
+);
 
-// const socket = socketConnection(
+socket.on("connect", () => {
+  console.log("connected to localhost:3000");
+
+  //Send connected workers to server
+  socket.emit("connectedWorkers", []);
+
+  //Get worker data (incl. actions) from server
+  socket.on("workerData", function (data: any) {
+    console.log("workerData from the server:", data);
+  });
+
+  //Function for debuggin websocket connection
+  setInterval(function () {
+    socket.emit("serverEvent", Math.random());
+    // socket.emit("getWorkerData");
+    console.log("message sent to the server");
+  }, 10000);
+});
+
+socket.on("clientEvent", function (data: any) {
+  console.log("message from the server:", data);
+  socket.emit("serverEvent", "thanks server! for sending '" + data + "'");
+});
+
+//Test data for scheduling
+// const workerData: IWorkerDto[] = [
 //   {
-//     backendHost: process.env.BACKEND_HOST!,
-//     backendPort: +process.env.BACKEND_PORT!,
+//     id: "id",
+
+//     name: "name",
+
+//     action: WorkerAction.PRESS,
+
+//     status: WorkerStatus.IDLE,
+
+//     state: WorkerState.ONLINE,
+
+//     actions: [
+//       {
+//         id: "action1",
+//         repeat: ActionRepeat.ONCE,
+
+//         executeDateTime: new Date(Date.now() + 5000),
+
+//         durationSeconds: 1,
+
+//         isComplete: false,
+//       },
+//       {
+//         id: "action1",
+//         repeat: ActionRepeat.ONCE,
+
+//         executeDateTime: new Date(Date.now() + 10000),
+
+//         durationSeconds: 1,
+
+//         isComplete: false,
+//       },
+//     ],
 //   },
-//   {
-//     hubId: process.env.HUB_ID!,
-//     hubSecret: process.env.HUB_SECRET!,
-//   }
-// );
-
-// socket.on("connect", () => {
-//   console.log("connected to localhost:3000");
-
-//   //Send connected workers to server
-//   socket.emit("connectedWorkers", []);
-
-//   //Get worker data (incl. actions) from server
-//   socket.on("workerData", function (data: any) {
-//     console.log("workerData from the server:", data);
-//   });
-
-//   //Function for debuggin websocket connection
-//   setInterval(function () {
-//     socket.emit("serverEvent", Math.random());
-//     // socket.emit("getWorkerData");
-//     console.log("message sent to the server");
-//   }, 10000);
-// });
-
-// socket.on("clientEvent", function (data: any) {
-//   console.log("message from the server:", data);
-//   socket.emit("serverEvent", "thanks server! for sending '" + data + "'");
-// });
+// ];
+// const sch = new scheduler();
+// sch.scheduleActions(workerData);
