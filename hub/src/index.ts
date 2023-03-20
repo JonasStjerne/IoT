41316c14090c9e33639eba2ca5eb35bd92c11335
Jonas Stjerne <jost@trifork.com>
2023-03-20 15:29:14 +0100

    Uncomment BLE code

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -40,70 +40,70 @@ socket.on("clientEvent", function (data: any) {
   socket.emit("serverEvent", "thanks server! for sending '" + data + "'");
 });
 
-// noble.on('stateChange', async (state) => {
-//   console.log(state);
-//   if (state === 'poweredOn') {
-//     await noble.startScanningAsync([]);
-//   }
-// });
+noble.on("stateChange", async (state) => {
+  console.log(state);
+  if (state === "poweredOn") {
+    await noble.startScanningAsync([]);
+  }
+});
 
-// noble.on("scanStart", () => {
-//   console.log("Started Scanning");
-// });
+noble.on("scanStart", () => {
+  console.log("Started Scanning");
+});
 
-// noble.on("warning", (warning: any) => {
-//   console.log(warning);
-// });
+noble.on("warning", (warning: any) => {
+  console.log(warning);
+});
 
-// noble.on("stateChange", function (state) {
-//   if (state === "poweredOn") {
-//     noble.startScanning(["19b10000-e8f2-537e-4f6c-d104768a1214"], true);
-//   } else {
-//     noble.stopScanning();
-//   }
-// });
+noble.on("stateChange", function (state) {
+  if (state === "poweredOn") {
+    noble.startScanning(["19b10000-e8f2-537e-4f6c-d104768a1214"], true);
+  } else {
+    noble.stopScanning();
+  }
+});
 
-// noble.on("discover", function (peripheral) {
-//   console.log(
-//     "peripheral discovered (" +
-//       peripheral.id +
-//       " with address <" +
-//       peripheral.address +
-//       ", " +
-//       peripheral.addressType +
-//       ">," +
-//       " connectable " +
-//       peripheral.connectable +
-//       "," +
-//       " RSSI " +
-//       peripheral.rssi +
-//       ":"
-//   );
-//   console.log("\thello my local name is:");
-//   console.log("\t\t" + peripheral.advertisement.localName);
-//   console.log("\tcan I interest you in any of the following advertised services:");
-//   console.log("\t\t" + JSON.stringify(peripheral.advertisement.serviceUuids));
+noble.on("discover", function (peripheral) {
+  console.log(
+    "peripheral discovered (" +
+      peripheral.id +
+      " with address <" +
+      peripheral.address +
+      ", " +
+      peripheral.addressType +
+      ">," +
+      " connectable " +
+      peripheral.connectable +
+      "," +
+      " RSSI " +
+      peripheral.rssi +
+      ":"
+  );
+  console.log("\thello my local name is:");
+  console.log("\t\t" + peripheral.advertisement.localName);
+  console.log("\tcan I interest you in any of the following advertised services:");
+  console.log("\t\t" + JSON.stringify(peripheral.advertisement.serviceUuids));
 
-//   var serviceData = peripheral.advertisement.serviceData;
-//   if (serviceData && serviceData.length) {
-//     console.log("\there is my service data:");
-//     for (var i in serviceData) {
-//       console.log(
-//         "\t\t" + JSON.stringify(serviceData[i].uuid) + ": " + JSON.stringify(serviceData[i].data.toString("hex"))
-//       );
-//     }
-//   }
-//   if (peripheral.advertisement.manufacturerData) {
-//     console.log("\there is my manufacturer data:");
-//     console.log("\t\t" + JSON.stringify(peripheral.advertisement.manufacturerData.toString("hex")));
-//   }
-//   if (peripheral.advertisement.txPowerLevel !== undefined) {
-//     console.log("\tmy TX power level is:");
-//     console.log("\t\t" + peripheral.advertisement.txPowerLevel);
-//   }
+  var serviceData = peripheral.advertisement.serviceData;
+  if (serviceData && serviceData.length) {
+    console.log("\there is my service data:");
+    for (var i in serviceData) {
+      console.log(
+        "\t\t" + JSON.stringify(serviceData[i].uuid) + ": " + JSON.stringify(serviceData[i].data.toString("hex"))
+      );
+    }
+  }
+  if (peripheral.advertisement.manufacturerData) {
+    console.log("\there is my manufacturer data:");
+    console.log("\t\t" + JSON.stringify(peripheral.advertisement.manufacturerData.toString("hex")));
+  }
+  if (peripheral.advertisement.txPowerLevel !== undefined) {
+    console.log("\tmy TX power level is:");
+    console.log("\t\t" + peripheral.advertisement.txPowerLevel);
+  }
 
-//   console.log();
-// });
+  console.log();
+});
 
 //Test data for scheduling
 // const workerData: IWorkerDto[] = [
