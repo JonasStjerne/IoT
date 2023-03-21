2cdbaec736a44cbfd6eca986c5ca24e9b879f2ca
Frederikke Filtenborg Michaelsen <77390652+FrederikkeFM@users.noreply.github.com>
2023-03-21 11:37:16 +0100

    Update index.ts

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -5,40 +5,40 @@ import { IWorkerDto, WorkerAction, WorkerState, WorkerStatus } from "./models/wo
 import { socketConnection } from "./websocket";
 import noble from "@abandonware/noble";
 
-const socket = socketConnection(
-  {
-    backendHost: "localhost",
-    backendPort: 3000,
-  },
-  {
-    hubId: process.env.HUB_ID!,
-    hubSecret: process.env.HUB_SECRET!,
-  }
-);
+// const socket = socketConnection(
+//   {
+//     backendHost: "localhost",
+//     backendPort: 3000,
+//   },
+//   {
+//     hubId: process.env.HUB_ID!,
+//     hubSecret: process.env.HUB_SECRET!,
+//   }
+// );
 
-socket.on("connect", () => {
-  console.log("connected to localhost:3000");
+// socket.on("connect", () => {
+//   console.log("connected to localhost:3000");
 
-  //Send connected workers to server
-  socket.emit("connectedWorkers", []);
+//   //Send connected workers to server
+//   socket.emit("connectedWorkers", []);
 
-  //Get worker data (incl. actions) from server
-  socket.on("workerData", function (data: any) {
-    console.log("workerData from the server:", data);
-  });
+//   //Get worker data (incl. actions) from server
+//   socket.on("workerData", function (data: any) {
+//     console.log("workerData from the server:", data);
+//   });
 
-  //Function for debuggin websocket connection
-  setInterval(function () {
-    socket.emit("serverEvent", Math.random());
-    // socket.emit("getWorkerData");
-    console.log("message sent to the server");
-  }, 10000);
-});
+//   //Function for debuggin websocket connection
+//   setInterval(function () {
+//     socket.emit("serverEvent", Math.random());
+//     // socket.emit("getWorkerData");
+//     console.log("message sent to the server");
+//   }, 10000);
+// });
 
-socket.on("clientEvent", function (data: any) {
-  console.log("message from the server:", data);
-  socket.emit("serverEvent", "thanks server! for sending '" + data + "'");
-});
+// socket.on("clientEvent", function (data: any) {
+//   console.log("message from the server:", data);
+//   socket.emit("serverEvent", "thanks server! for sending '" + data + "'");
+// });
 
 noble.on("stateChange", async (state) => {
   console.log(state);
