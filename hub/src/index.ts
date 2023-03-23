8941d028b5cdc2eeb3abfa50cf338a1eae0b6e7e
Jonas Stjerne <jost@trifork.com>
2023-03-23 13:15:56 +0100

    redo structure

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -5,6 +5,102 @@ import { IWorkerDto, WorkerAction, WorkerState, WorkerStatus } from "./models/wo
 import { socketConnection } from "./websocket";
 import noble, { Characteristic } from "@abandonware/noble";
 
+const SERVICE_UUID = "19b10000e8f2537e4f6cd104768a1214";
+const BATTERY_SERVICE = "180f";
+noble.on("scanStart", () => {
+  console.log("Started Scanning");
+});
+
+noble.on("warning", (warning: any) => {
+  console.log(warning);
+});
+
+noble.on("stateChange", function (state) {
+  if (state === "poweredOn") {
+    noble.startScanning([SERVICE_UUID, BATTERY_SERVICE], true);
+  } else {
+    noble.stopScanning();
+  }
+});
+
+function logPeripheral(peripheral: noble.Peripheral) {
+  console.log(
+    "peripheral discovered (" +
+      peripheral.id +
+      " with address <" +
+      peripheral.address +
+      ", " +
+      peripheral.addressType +
+      ">," +
+      " connectable " +
+      peripheral.connectable +
+      "," +
+      " RSSI " +
+      peripheral.rssi +
+      ":"
+  );
+  console.log("\thello my local name is:");
+  console.log("\t\t" + peripheral.advertisement.localName);
+  console.log("\tcan I interest you in any of the following advertised services:");
+  console.log("\t\t" + JSON.stringify(peripheral.advertisement.serviceUuids));
+}
+
+//Event for when a peripheral is discovered
+
+noble.on("discover", function (peripheral) {
+  logPeripheral(peripheral);
+
+  //stop scanning before connecting
+  noble.stopScanning();
+  connect(peripheral);
+});
+
+//Connect to the peripheral
+function connect(peripheral: noble.Peripheral) {
+  peripheral.on("disconnect", function () {
+    process.exit(0);
+  });
+
+  peripheral.once("connect", () => {
+    console.log("Connected!!!");
+    peripheral.discoverSomeServicesAndCharacteristics(
+      [SERVICE_UUID, BATTERY_SERVICE],
+      [],
+      discoveredServicesAndCharacteristics
+    );
+  });
+
+  peripheral.connect();
+}
+
+function discoveredServicesAndCharacteristics(
+  error: string,
+  services: noble.Service[],
+  characteristics: noble.Characteristic[]
+) {
+  console.log("Services ", services);
+  console.log("Characteristics ", characteristics);
+  characteristics.forEach((characteristic) => {
+    switch (characteristic.uuid) {
+      case "19b10001e8f2537e4f6cd104768a1214":
+        characteristic.write(Buffer.alloc(1, 1, "binary"), false);
+        break;
+      case "2a19":
+        characteristic.on("data", (data, isNotifaction) => {
+          console.log("Battery level: ", data);
+        });
+        characteristic.subscribe((error) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Subscribed to battery");
+          }
+        });
+        break;
+    }
+  });
+}
+
 // const socket = socketConnection(
 //   {
 //     backendHost: "localhost",
@@ -39,114 +135,6 @@ import noble, { Characteristic } from "@abandonware/noble";
 //   console.log("message from the server:", data);
 //   socket.emit("serverEvent", "thanks server! for sending '" + data + "'");
 // });
-const SERVICE_UUID = "19b10000e8f2537e4f6cd104768a1214";
-const BATTERY_SERVICE = "180f";
-noble.on("scanStart", () => {
-  console.log("Started Scanning");
-});
-
-noble.on("warning", (warning: any) => {
-  console.log(warning);
-});
-
-noble.on("stateChange", function (state) {
-  if (state === "poweredOn") {
-    noble.startScanning([SERVICE_UUID, BATTERY_SERVICE], true);
-  } else {
-    noble.stopScanning();
-  }
-});
-
-noble.on("discover", function (peripheral) {
-  console.log(
-    "peripheral discovered (" +
-      peripheral.id +
-      " with address <" +
-      peripheral.address +
-      ", " +
-      peripheral.addressType +
-      ">," +
-      " connectable " +
-      peripheral.connectable +
-      "," +
-      " RSSI " +
-      peripheral.rssi +
-      ":"
-  );
-  console.log("\thello my local name is:");
-  console.log("\t\t" + peripheral.advertisement.localName);
-  console.log("\tcan I interest you in any of the following advertised services:");
-  console.log("\t\t" + JSON.stringify(peripheral.advertisement.serviceUuids));
-
-  var serviceData = peripheral.advertisement.serviceData;
-  if (serviceData && serviceData.length) {
-    console.log("\there is my service data:");
-    for (var i in serviceData) {
-      console.log(
-        "\t\t" + JSON.stringify(serviceData[i].uuid) + ": " + JSON.stringify(serviceData[i].data.toString("hex"))
-      );
-    }
-  }
-  if (peripheral.advertisement.manufacturerData) {
-    console.log("\there is my manufacturer data:");
-    console.log("\t\t" + JSON.stringify(peripheral.advertisement.manufacturerData.toString("hex")));
-  }
-  if (peripheral.advertisement.txPowerLevel !== undefined) {
-    console.log("\tmy TX power level is:");
-    console.log("\t\t" + peripheral.advertisement.txPowerLevel);
-  }
-  //stop scanning before connecting
-  noble.stopScanning();
-  connect(peripheral);
-});
-
-function connect(peripheral: noble.Peripheral) {
-  peripheral.on("disconnect", function () {
-    process.exit(0);
-  });
-
-  peripheral.connect(() => {
-    console.log("Connecting");
-
-    peripheral.discoverServices([SERVICE_UUID, BATTERY_SERVICE], (error, services) => {
-      console.log("Services ", services);
-
-      services.forEach((service) => {
-        console.log(service.uuid);
-        service.discoverCharacteristics([], (error, characteristics) => {
-          console.log(characteristics);
-          if (characteristics[0].uuid === "19b10001e8f2537e4f6cd104768a1214") {
-            characteristics[0].write(Buffer.alloc(1, 1, "binary"), false);
-
-            characteristics[0].once("write", (error: any) => {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log("Wrote to gesture");
-              }
-            });
-          } else {
-            console.log("Battery characteristic found");
-            console.log(characteristics[0]);
-            characteristics[0].on("data", (data, isNotifaction) => {
-              console.log("Battery level: ", data);
-            });
-            characteristics[0].subscribe((error) => {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log("Subscribed to battery");
-              }
-            });
-          }
-        });
-      });
-    });
-  });
-  peripheral.once("connect", () => {
-    console.log("Connected!!!");
-  });
-}
 
 //Test data for scheduling
 // const workerData: IWorkerDto[] = [
