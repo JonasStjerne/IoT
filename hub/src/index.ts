75234c3385e86a40e6d3f93279c34b6106bcc066
Jonas Stjerne <jost@trifork.com>
2023-03-23 13:25:09 +0100

    Update index.ts

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -6,8 +6,8 @@ import { socketConnection } from "./websocket";
 import noble, { Characteristic } from "@abandonware/noble";
 
 const SERVICE_UUID = "19b10000e8f2537e4f6cd104768a1214";
-const BATTERY_SERVICE = "180f";
-const BATTERY_CHAR = "2a19";
+const BATTERY_SERVICE = "19b10000e8f2537e4f6cd104768a1200";
+const BATTERY_CHAR = "19b10000e8f2537e4f6cd104768a1299";
 noble.on("scanStart", () => {
   console.log("Started Scanning");
 });
@@ -66,7 +66,7 @@ function connect(peripheral: noble.Peripheral) {
     console.log("Connected!!!");
     peripheral.discoverSomeServicesAndCharacteristics(
       [SERVICE_UUID, BATTERY_SERVICE],
-      [BATTERY_CHAR],
+      [],
       discoveredServicesAndCharacteristics
     );
   });
