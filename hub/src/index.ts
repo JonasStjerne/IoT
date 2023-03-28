da7fd34ec245ea1d963184bf9ef0244965c214dc
Jonas Stjerne <jost@trifork.com>
2023-03-28 13:39:51 +0200

    Update index.ts

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -4,97 +4,23 @@ import { ActionRepeat } from "./models/action.dto";
 import { IWorkerDto, WorkerAction, WorkerState, WorkerStatus } from "./models/worker.dto";
 import { socketConnection } from "./websocket";
 import noble from "@abandonware/noble";
+import bluetoothService from "./bluetooth";
 
-const SERVICE_UUID = "19b10000e8f2537e4f6cd104768a1214";
-const BATTERY_SERVICE = "180f";
-const BATTERY_CHAR = "2a19";
+const ACTION_SERVICE_UUID = "19b10000e8f2537e4f6cd104768a1214";
+const ACTION_CHAR_UUID = "19b10001e8f2537e4f6cd104768a1214";
+const BATTERY_SERVICE_UUID = "180f";
+const BATTERY_CHAR_UUID = "2a19";
 
-noble.on("warning", (warning: any) => {
-  console.log(warning);
-});
+const BluetoothService = new bluetoothService(
+  [ACTION_SERVICE_UUID, BATTERY_SERVICE_UUID],
+  BATTERY_CHAR_UUID,
+  ACTION_CHAR_UUID
+);
 
-noble.on("stateChange", function (state) {
-  if (state === "poweredOn") {
-    noble.startScanning([SERVICE_UUID, BATTERY_SERVICE], true);
-  } else {
-    noble.stopScanning();
-  }
-});
-
-function logPeripheral(peripheral: noble.Peripheral) {
-  console.log(
-    "peripheral discovered (" +
-      peripheral.id +
-      " with address <" +
-      peripheral.address +
-      ", " +
-      peripheral.addressType +
-      ">," +
-      " connectable " +
-      peripheral.connectable +
-      "," +
-      " RSSI " +
-      peripheral.rssi +
-      ":"
-  );
-  console.log("\thello my local name is:");
-  console.log("\t\t" + peripheral.advertisement.localName);
-  console.log("\tcan I interest you in any of the following advertised services:");
-  console.log("\t\t" + JSON.stringify(peripheral.advertisement.serviceUuids));
-}
-
-//Event for when a peripheral is discovered
-
-noble.on("discover", function (peripheral) {
-  logPeripheral(peripheral);
-
-  //stop scanning before connecting
-  noble.stopScanning();
-  connect(peripheral);
-});
-
-//Connect to the peripheral
-function connect(peripheral: noble.Peripheral) {
-  peripheral.on("disconnect", function () {
-    process.exit(0);
-  });
-
-  peripheral.once("connect", () => {
-    console.log("Connected!!!");
-    peripheral.discoverSomeServicesAndCharacteristics([BATTERY_SERVICE], [], discoveredServicesAndCharacteristics);
-  });
-
-  peripheral.connect();
-}
-
-function discoveredServicesAndCharacteristics(
-  error: string,
-  services: noble.Service[],
-  characteristics: noble.Characteristic[]
-) {
-  characteristics.forEach((characteristic) => {
-    console.log("Characteristics ", characteristic.uuid);
-    switch (characteristic.uuid) {
-      case "19b10001e8f2537e4f6cd104768a1214":
-        characteristic.write(Buffer.alloc(1, 1, "binary"), false);
-        break;
-      case BATTERY_CHAR:
-        console.log("case ran");
-        characteristic.on("data", (data, isNotifaction) => {
-          console.log("Battery level: ", data[0], isNotifaction);
-        });
-        // characteristic.read((error, data) => {
-        //   console.log("Battery level: ", data[0]);
-        // });
-        characteristic.subscribe(() => {
-          console.log("Subscribed to battery level");
-        });
-
-        break;
-    }
-  });
-}
-
+setInterval(async () => {
+  const batteryData = await BluetoothService.getBatteryLevel();
+  console.log(batteryData);
+}, 1000);
 // const socket = socketConnection(
 //   {
 //     backendHost: "localhost",
