c730ee15fdcaadb8f54261a582a5c88a540dffb6
Frederikke Filtenborg Michaelsen <77390652+FrederikkeFM@users.noreply.github.com>
2023-04-04 12:16:04 +0200

    instant action button

--- frontend/src/app/_pages/worker/worker.component.ts
+++ frontend/src/app/_pages/worker/worker.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Action } from 'src/app/_api/models';
+import { ActionApiService } from 'src/app/_api/services';
 
 @Component({
   selector: 'app-worker',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./worker.component.css']
 })
 export class WorkerComponent implements OnInit {
+  workerId: string | null = null;
+  
 
-  constructor() { }
+  constructor(
+    private actionApiService: ActionApiService,
+    private toastService: ToastrService,
+    private route: ActivatedRoute,) { }
+
+    actions: Action[] = [];
 
   ngOnInit(): void {
+    this.workerId = this.route.snapshot.paramMap.get('workerId');
+    
+    if (this.workerId) {
+      this.actionApiService.actionControllerFindAll({ id: this.workerId}).subscribe({
+        next: (res) => {
+          this.actions = res;
+        },
+      }); 
+    }
+  }
+ 
+  instantAction() {
+    this.actionApiService
+      .actionControllerSendInstantAction ({ workerId: this.workerId! })
+      .subscribe({
+        next: (res) => {
+          this.toastService.success('Instant action activated');
+        },
+      });
   }
 
 }
+
